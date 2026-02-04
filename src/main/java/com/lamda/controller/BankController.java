c2319c07aac07963177ab4fa58e891814abd7a12
Prabhakaran G <prabhakaransaai@gmail.com>
2026-02-04 15:43:29 +0530

    error commit

--- src/main/java/com/lamda/controller/BankController.java
+++ src/main/java/com/lamda/controller/BankController.java
@@ -1,5 +1,7 @@
 package com.lamda.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bank")
 public class BankController {
-	
+	private static final Logger log = LoggerFactory.getLogger(BankController.class);
 	@GetMapping("/getData")
 	public ResponseEntity<String> getData(){
+		log.info("API called");
 		return ResponseEntity.status(HttpStatus.OK).body("Lambda Succesfully Deployed and tested");
 	}
 
